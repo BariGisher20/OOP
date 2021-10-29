6fa06986eff50f6cb33b5d5a0d0fb1c2d3c246e6
Eleonora Harutyunyan <ev.arutyunyan@mail.ru>
2021-10-29 16:10:46 +0300

    Исправленная

--- main.py
+++ main.py
@@ -13,6 +13,13 @@ def lines_quantity(file_name):
         return n
 
 
+def create_dict(file_name):
+    dict_line = []
+    a = lines_quantity(file_name)
+    dict_line.append(a)
+    return sorted(dict_line)
+
+
 def file_content(file_name):
     with open(file_name, "r", encoding="utf-8") as f:
         content = f.read()
@@ -21,13 +28,16 @@ def file_content(file_name):
 
 def write(file_name):
     with open(file_name, "w", encoding="utf-8") as file:
-        for files in res:
+        for files in res[::-1]:
             if files.endswith(".txt"):
                 keys = files
                 values = lines_quantity(files)
-                text = file_content(files)
-                res_3 = f'{keys}\n {values}\n {text}\n\n'
-                file.write(res_3)
+                pprint(values)
+                if values >0:
+                    text = file_content(files)
+                    res_3 = f'{keys}\n {values}\n {text}\n\n'
+
+                    file.write(res_3)
 
 
 pprint(write('new.txt'))
